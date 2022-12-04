7c0ea94deb5463236f4502d0030dddc1aff2ba76
André Canal <93547260+adorigueto@users.noreply.github.com>
2022-12-04 12:26:48 -0300

    wine_rain

--- wine.r
+++ wine.r
@@ -1,5 +1,3 @@
-setwd("C:/Coding/R/05_Wine_Reviews")
-wine <- read.csv("C:/Coding/R/05_Wine_Reviews/winemag-data-130k-v2.csv/winemag-data-130k-v2.csv")
 #renv::init()
 
 library(ggplot2)
@@ -7,6 +5,10 @@ library(viridis)
 library(tidyr)
 library(ggdist)
 library(tidyverse)
+library(tidyquant)
+
+setwd("C:/Coding/R/05_Wine_Reviews")
+wine <- read.csv("C:/Coding/R/05_Wine_Reviews/winemag-data-130k-v2.csv/winemag-data-130k-v2.csv")
 
 head(wine)
 nrow(wine)
@@ -42,13 +44,15 @@ ggplot(wine_wo_out_subset_countries,
   geom_boxplot(width=0.05) +
   annotate(geom="text", x=2.5, y=0, label="* Outliers removed from sample")
 
+# Generate image with the plot
+png("wine_rain.png")
 
-png("wine_rain.png", width = 1000, #height = 600,
-    res = 250, pointsize = 1)
 wine_wo_out_subset_countries %>%
   
+  # Plot functions
   ggplot(aes(x = country, y = price, fill = country)) +
   
+  # Half a violin plot
   ggdist::stat_halfeye(
     adjust = 0.5,
     justification = -.2,
@@ -56,12 +60,14 @@ wine_wo_out_subset_countries %>%
     point_color = NA
   ) +
   
+  # Boxplots in the inside
   geom_boxplot(
     width = .12,
     outlier.color = NA,
     alpha = 0.5
   ) +
   
+  # Rain drops with stat_dots
   ggdist::stat_dots(
     side = "left",
     justification = 1.1,
@@ -69,9 +75,12 @@ wine_wo_out_subset_countries %>%
     stackratio = 0.01,
     layout = "bin"
   ) +
+  
+  # Overall theme
   tidyquant::scale_fill_tq() +
   tidyquant::theme_tq() +
-  theme(legend.position = "none") +
+  theme(legend.position = "none",
+        text = element_text(size = 4)) +
   coord_flip()
 dev.off()
 
